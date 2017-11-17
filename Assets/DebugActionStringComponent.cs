2a4bafe5e2c61c2a2577be97b58737013b8c0146
Ruben Garat <ruben@ironhidegames.com>
2017-11-17 18:14:02 -0300

    initial import

---
+++ Assets/DebugActionStringComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class DebugActionStringComponent : MonoBehaviour
+{
+    public string prefix;
+
+    public void Execute(string message)
+    {
+        Debug.LogFormat("{0} - {1}", prefix, message);
+    }
+}
---
+++ Assets/Invoker.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Invoker : MonoBehaviour 
+{
+
+	public UnityEvent action;
+
+	[ContextMenu("Execute")]
+	public void Execute(){
+		action.Invoke ();
+	}
+}
